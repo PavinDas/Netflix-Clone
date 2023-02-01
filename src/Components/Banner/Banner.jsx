4b0f3d2169f320b77e8ca4d22fee71f4ea85aa5d
Pavin Das <pavindas007@gmail.com>
2023-02-01 23:07:57 +0530

    Added Navbar,banner,Rowpost

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Banner.css"
+
+function Banner() {
+  return (
+    <div className='banner'>
+      <div className='content'>
+        <h1 className='title'>Movie Name</h1>
+        <div className='banner-buttons'>
+            <button className='button' >Play</button>
+            <button className='button' >My list</button>
+        </div>
+        <h1 className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque illo iste aliquid ipsum illum! Enim similique possimus soluta provident</h1>
+
+      </div>
+    <div className="fade-bottom">
+      
+    </div>
+    </div>
+  )
+}
+
+export default Banner
